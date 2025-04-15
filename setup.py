997f46f7f4b9c5243552f0cc02002776d0c302b2
sanika jadhav <sanika@gmail.com>
2025-04-15 17:09:35 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'DR.S3',
+    version= '0.0.0',
+    author= 'Sanika Saif & Sourabh',
+    packages= find_packages(),
+    install_requires = []
+
+)
